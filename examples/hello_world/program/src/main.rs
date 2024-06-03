28de577756ce1eb5464986c1b60872f3b9d4c131
Lane Rettig <lanerettig@gmail.com>
2024-06-03 13:22:40 -0700

    Simplify examples
    hello_world example is running successfully

--- examples/hello_world/program/src/main.rs
+++ examples/hello_world/program/src/main.rs
@@ -1,35 +1,6 @@
-// #![no_std]
 #![no_main]
 athena_vm::entrypoint!(main);
 
-// #[no_mangle]
-// pub extern "C" fn _start() -> ! {
-//     let a: u32 = 5;
-//     let b: u32 = 3;
-//     let sum = add(a, b);
-
-//     // We need a way to output `sum` for verification
-//     // Assuming a function `output` exists to demonstrate
-//     // output(sum);
-// }
-
 pub fn main() {
-    let a: u32 = 5;
-    let b: u32 = 3;
-    let _sum = add(a, b);
-
-    // We need a way to output `sum` for verification
-    // Assuming a function `output` exists to demonstrate
-    // output(sum);
+    println!("Hello, world!");
 }
-
-fn add(x: u32, y: u32) -> u32 {
-    x + y
-}
-
-// #[panic_handler]
-// fn panic(_info: &core::panic::PanicInfo) -> ! {
-//     unsafe {
-//       core::arch::asm!("unimp", options(noreturn));
-//     }
-// }
