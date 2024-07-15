de1e0d74335cd4f73857ed8849fbc3a4f173f43e
Lane Rettig <lanerettig@gmail.com>
2024-07-15 16:18:56 -0700

    Update vergen dependency

--- cli/build.rs
+++ cli/build.rs
@@ -1,7 +1,13 @@
-fn main() {
-    vergen::EmitBuilder::builder()
-        .build_timestamp()
-        .git_sha(true)
-        .emit()
-        .unwrap();
+use anyhow::Result;
+use vergen_git2::{BuildBuilder, Emitter, Git2Builder};
+
+fn main() -> Result<()> {
+  let build = BuildBuilder::default().build_timestamp(true).build()?;
+  let git2 = Git2Builder::default().sha(true).build()?;
+
+  Emitter::default()
+    .add_instructions(&build)?
+    .add_instructions(&git2)?
+    .emit()?;
+  Ok(())
 }
