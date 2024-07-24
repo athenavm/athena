d69f6303cacce77ab08e20dc8b56ed35f534a0cb
Lane Rettig <lanerettig@gmail.com>
2024-07-24 16:17:42 -0700

    Adding Go binding tests

---
+++ ffi/athcon/bindings/go/athcon_test.go
@@ -0,0 +1,81 @@
+//go:generate g++ -shared ../../../examples/example_vm/example_vm.cpp -I../../../include -o example_vm.so
+
+package athcon
+
+import (
+	"bytes"
+	"testing"
+)
+
+var modulePath = "./example_vm.so"
+
+func TestLoad(t *testing.T) {
+	i, err := Load(modulePath)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	defer i.Destroy()
+	if i.Name() != "example_vm" {
+		t.Fatalf("name is %s", i.Name())
+	}
+	if i.Version()[0] < '0' || i.Version()[0] > '9' {
+		t.Fatalf("version number is weird: %s", i.Version())
+	}
+}
+
+func TestLoadConfigure(t *testing.T) {
+	i, err := LoadAndConfigure(modulePath)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	defer i.Destroy()
+	if i.Name() != "example_vm" {
+		t.Fatalf("name is %s", i.Name())
+	}
+	if i.Version()[0] < '0' || i.Version()[0] > '9' {
+		t.Fatalf("version number is weird: %s", i.Version())
+	}
+}
+
+func TestExecuteEmptyCode(t *testing.T) {
+	vm, _ := Load(modulePath)
+	defer vm.Destroy()
+
+	addr := Address{}
+	h := Bytes32{}
+	result, err := vm.Execute(nil, Frontier, Call, 1, 999, addr, addr, nil, h, nil)
+
+	if !bytes.Equal(result.Output, []byte("")) {
+		t.Errorf("execution unexpected output: %x", result.Output)
+	}
+	if result.GasLeft != 999 {
+		t.Errorf("execution gas left is incorrect: %d", result.GasLeft)
+	}
+	if err != nil {
+		t.Errorf("execution returned unexpected error: %v", err)
+	}
+}
+
+func TestRevision(t *testing.T) {
+	if MaxRevision != Frontier {
+		t.Errorf("missing constant for revision %d", MaxRevision)
+	}
+	if LatestStableRevision != Frontier {
+		t.Errorf("wrong latest stable revision %d", LatestStableRevision)
+	}
+}
+
+func TestErrorMessage(t *testing.T) {
+
+	check := func(err Error, expectedMsg string) {
+		if err.Error() != expectedMsg {
+			t.Errorf("wrong error message: '%s', expected: '%s'", err.Error(), expectedMsg)
+		}
+	}
+
+	check(Failure, "failure")
+	check(Revert, "revert")
+	check(Error(3), "out of gas")
+	check(Error(-1), "internal error")
+	check(Error(1000), "<unknown>")
+}
---
+++ ffi/athcon/bindings/go/host_test.go
@@ -0,0 +1,100 @@
+package athcon
+
+import (
+	"bytes"
+	"testing"
+)
+
+type testHostContext struct{}
+
+func (host *testHostContext) AccountExists(addr Address) bool {
+	return false
+}
+
+func (host *testHostContext) GetStorage(addr Address, key Bytes32) Bytes32 {
+	return Bytes32{}
+}
+
+func (host *testHostContext) SetStorage(addr Address, key Bytes32, value Bytes32) (status StorageStatus) {
+	return StorageAssigned
+}
+
+func (host *testHostContext) GetBalance(addr Address) Bytes32 {
+	return Bytes32{}
+}
+
+func (host *testHostContext) GetTxContext() TxContext {
+	txContext := TxContext{}
+	txContext.BlockHeight = 42
+	return txContext
+}
+
+func (host *testHostContext) GetBlockHash(number int64) Bytes32 {
+	return Bytes32{}
+}
+
+func (host *testHostContext) Call(kind CallKind,
+	recipient Address, sender Address, value Bytes32, input []byte, gas int64, depth int) (
+	output []byte, gasLeft int64, createAddr Address, err error) {
+	output = []byte("output from testHostContext.Call()")
+	return output, gas, Address{}, nil
+}
+
+func TestGetBlockHeightFromTxContext(t *testing.T) {
+	// Yul: mstore(0, number()) return(0, msize())
+	code := []byte("\x43\x60\x00\x52\x59\x60\x00\xf3")
+
+	vm, _ := Load(modulePath)
+	defer vm.Destroy()
+
+	host := &testHostContext{}
+	addr := Address{}
+	h := Bytes32{}
+	result, err := vm.Execute(host, Frontier, Call, 1, 100, addr, addr, nil, h, code)
+	output := result.Output
+	gasLeft := result.GasLeft
+
+	if len(output) != 32 {
+		t.Errorf("unexpected output size: %d", len(output))
+	}
+
+	// Should return value 42 (0x2a) as defined in GetTxContext().
+	expectedOutput := []byte("\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x2a")
+	if !bytes.Equal(output, expectedOutput) {
+		t.Errorf("execution unexpected output: %x", output)
+	}
+	if gasLeft != 94 {
+		t.Errorf("execution gas left is incorrect: %d", gasLeft)
+	}
+	if err != nil {
+		t.Errorf("execution returned unexpected error: %v", err)
+	}
+}
+
+func TestCall(t *testing.T) {
+	// pseudo-Yul: call(0, 0, 0, 0, 0, 0, 34) return(0, msize())
+	code := []byte("\x60\x22\x60\x00\x80\x80\x80\x80\x80\xf1\x59\x60\x00\xf3")
+
+	vm, _ := Load(modulePath)
+	defer vm.Destroy()
+
+	host := &testHostContext{}
+	addr := Address{}
+	h := Bytes32{}
+	result, err := vm.Execute(host, Frontier, Call, 1, 100, addr, addr, nil, h, code)
+	output := result.Output
+	gasLeft := result.GasLeft
+
+	if len(output) != 34 {
+		t.Errorf("execution unexpected output length: %d", len(output))
+	}
+	if !bytes.Equal(output, []byte("output from testHostContext.Call()")) {
+		t.Errorf("execution unexpected output: %s", output)
+	}
+	if gasLeft != 89 {
+		t.Errorf("execution gas left is incorrect: %d", gasLeft)
+	}
+	if err != nil {
+		t.Errorf("execution returned unexpected error: %v", err)
+	}
+}
