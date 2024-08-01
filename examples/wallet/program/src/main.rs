c8d3a33a1e28901eea8b4560f31876e6b52fb6c2
Lane Rettig <lanerettig@gmail.com>
2024-08-01 12:50:45 -0700

    So far so good
    Things are working so far
    Calling the methods in main() ensures they appear in the symbol table

--- examples/wallet/program/src/main.rs
+++ examples/wallet/program/src/main.rs
@@ -5,12 +5,20 @@ athena_vm::entrypoint!(main);
 use athena_interface::{Address, Balance};
 use athena_vm_declare::export;
 use athena_vm_sdk::{call, Pubkey, VerifiableTemplate, WalletProgram};
-use borsh::from_slice;
+use borsh::{from_slice, to_vec};
 use borsh_derive::{BorshDeserialize, BorshSerialize};
 use ed25519_dalek::{Signature, Verifier, VerifyingKey};
 // use wallet_common::{SendArguments, SpawnArguments};
 
-pub fn main() {}
+pub fn main() {
+  let wallet = Wallet::new([0u8; 32]);
+  Wallet::spawn([0u8; 32]);
+  wallet.send([0u8; 24], 0);
+  Wallet::athexp_spawn([0u8; 32]);
+  let serialized = to_vec(&wallet).unwrap();
+  let (state, statelen) = (serialized.as_ptr(), serialized.len());
+  Wallet::athexp_send(state, statelen, [0u8; 24], 0);
+}
 
 #[derive(BorshSerialize, BorshDeserialize)]
 pub struct Wallet {
@@ -38,23 +46,24 @@ impl Wallet {
 }
 
 impl WalletProgram for Wallet {
-  fn send(self, recipient: Address, amount: Balance) {
+  #[export]
+  fn send(&self, recipient: Address, amount: Balance) {
     // Send coins
     // Note: error checking happens inside the host
     call(recipient, None, amount);
   }
 
-  fn proxy(self, _destination: Address, _args: Vec<u8>) {
+  fn proxy(&self, _destination: Address, _args: Vec<u8>) {
     unimplemented!();
   }
 
-  fn deploy(self, _code: Vec<u8>) {
+  fn deploy(&self, _code: Vec<u8>) {
     unimplemented!();
   }
 }
 
 impl VerifiableTemplate for Wallet {
-  fn verify(self, tx: &[u8], signature: &[u8; 64]) -> bool {
+  fn verify(&self, tx: &[u8], signature: &[u8; 64]) -> bool {
     // Check that the transaction is signed by the owner
     let public_key = VerifyingKey::from_bytes(&self.owner).unwrap();
     let signature = Signature::from_bytes(signature);
