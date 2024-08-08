66186132f08076f6c3c207df7c7b6cfab9710707
Lane Rettig <lanerettig@gmail.com>
2024-08-08 12:03:25 -0400

    Update wallet to call spawn
    Correctly serialize/deserialize fn args

--- examples/wallet/common/src/lib.rs
+++ examples/wallet/common/src/lib.rs
@@ -1,23 +1,22 @@
 use athena_interface::Address;
-use athena_vm_sdk::Pubkey;
-use parity_scale_codec::{Decode, Encode};
+use borsh_derive::{BorshDeserialize, BorshSerialize};
 
-#[derive(Encode, Decode)]
+#[derive(BorshSerialize, BorshDeserialize)]
 pub struct SendArguments {
   pub recipient: Address,
   pub amount: u64,
 }
 
-#[derive(Encode, Decode)]
-pub struct SpawnArguments {
-  pub owner: Pubkey,
-}
+// #[derive(Encode, Decode)]
+// pub struct SpawnArguments {
+//   pub owner: Pubkey,
+// }
 
 // The method selectors
-#[derive(PartialEq)]
-pub enum MethodId {
-  Spawn = 0,
-  Send = 1,
-  Proxy = 2,
-  Deploy = 3,
-}
+// #[derive(PartialEq)]
+// pub enum MethodId {
+//   Spawn = 0,
+//   Send = 1,
+//   Proxy = 2,
+//   Deploy = 3,
+// }
