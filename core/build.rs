2af954859f29ab42213851c436e74d419b4345a4
Bartosz Różański <bartek.roza@gmail.com>
2024-10-17 15:19:06 +0200

    remove test and example binaries
    Unittests that need elfs now build them automatically

---
+++ core/build.rs
@@ -0,0 +1,17 @@
+use athena_helper::build_program;
+
+fn build_programs_for_tests() {
+  build_program("../examples/fibonacci/program");
+  build_program("../examples/hello_world/program");
+  build_program("../examples/io/program");
+  build_program("../examples/wallet/program");
+  build_program("../tests/fibonacci");
+  build_program("../tests/host");
+  build_program("../tests/hint-io");
+  build_program("../tests/panic");
+}
+
+fn main() {
+  #[cfg(feature = "unittest")]
+  build_programs_for_tests()
+}
