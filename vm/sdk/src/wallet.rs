ecc6714880ebaf6e57caac7a3bbdebcbe29e2944
Bartosz Różański <bartek.roza@gmail.com>
2025-01-03 16:12:53 +0100

    implement wallet example proxy method

--- vm/sdk/src/wallet.rs
+++ vm/sdk/src/wallet.rs
@@ -11,10 +11,18 @@ pub struct SpendArguments {
   pub amount: u64,
 }
 
+#[derive(Clone, Debug, Encode, Decode, PartialEq, Eq)]
+pub struct ProxyArguments {
+  pub destination: Address,
+  pub method: Option<MethodSelector>,
+  pub args: Option<Vec<u8>>,
+  pub amount: u64,
+}
+
 pub trait WalletProgram {
   fn spawn(owner: Pubkey) -> Address;
   fn spend(&self, args: SpendArguments);
-  fn proxy(&self, destination: Address, args: &[u8]);
+  fn proxy(&self, args: ProxyArguments) -> Vec<u8>;
   fn deploy(&self, code: Vec<u8>) -> Address;
   fn max_spend(&self, args: SpendArguments) -> u64;
 }
